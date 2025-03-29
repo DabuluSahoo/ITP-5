089281f617aae37fb78b79b0101c28dad7470d0f
Swagat-1655 <swagatsrinivashmajhi@gmail.com>
2025-03-29 12:08:42 +0530

    new file added

---
+++ Swagat.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main() 
+{
+   std:: cout<<"Name: Shree Swagat Srinivash Majhi"<<std::endl;
+   std:: cout<<"Regd No.: 2402041156"<<std::endl;
+   std:: cout<<"Branch: Computer Science and Engineering"<<std::endl;
+
+   return 0;
+} 
